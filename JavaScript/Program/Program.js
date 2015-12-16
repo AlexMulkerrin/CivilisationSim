00f113154e2d591ddfa7604b6ae8d3ec02d5f779
Alex Mulkerrin <a.mulkerrin.12@aberdeen.ac.uk>
2015-12-16 10:07:16 +0000

    starting on loading map from file

--- JavaScript/Program/Program.js
+++ JavaScript/Program/Program.js
@@ -14,9 +14,20 @@ function Program(canvasName) {
 	this.control = new Control(canvasName, this.simulation, this.display);
 	this.display.linkControl(this.control);
 
+	this.loadMapImage("world map small");
+
 }
 
 Program.prototype.update = function() {
 	this.display.update();
 	this.control.mouse.isReleased = false;
 }
+
+Program.prototype.loadMapImage = function (imageName) {
+	var image = new Image();
+	image.src = "Resources/"+imageName+".png";
+	var t = this;
+	image.onload = function() {
+		t.simulation.setMapFromImage(image);
+	}
+};
--- JavaScript/Simulation/Simulation.js
+++ JavaScript/Simulation/Simulation.js
@@ -7,3 +7,10 @@ function Simulation(w,h) {
 	//this.radius = Math.floor(this.width/2);
 	this.terrain = new TerrainGenerator(this.width, this.height);
 }
+
+Simulation.prototype.setMapFromImage = function (image) {
+	var index, r, g, b;
+	var colourString;
+
+	var tempCanvas = document.createElement('canvas');
+};
